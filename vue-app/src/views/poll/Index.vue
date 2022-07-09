1608491c85ad6fc8eab90492f3092f0652114ff3
korban-mantan <mubarokakbar13@gmail.com>
2022-07-09 13:11:27 +0700

    minor changes

--- vue-app/src/views/poll/Index.vue
+++ vue-app/src/views/poll/Index.vue
@@ -3,23 +3,27 @@
         <div class="row justify-content-center">
             <div class="col-8">
                 <route-link :to="{name: 'poll.create'}" class="btn btn-primary btn-sm rounded shadow mb-3">Add</route-link>
-
                 <div class="card rounded shadow">
                     <div class="card-header">
+                        Poll list:
 
                     </div>
                     <div class="card-body">
                         <table class="table">
                             <thead>
-                                <tr>Title</tr>
-                                <tr>Description</tr>
-                                <tr>Body</tr>
+                                <tr>
+                                    <th>Title</th>
+                                    <th>Description</th>
+                                    <th>Body</th>
+                                    <th>Upload Time</th>
+                                </tr>
                             </thead>
                             <tbody>
-                                <tr>
+                                <tr v-for="(poll, index) in poll.data" :key="index">
                                     <td>{{poll.title}}</td>
                                     <td>{{poll.desc}}</td>
                                     <td>{{poll.body}}</td>
+                                    <td>{{poll.upload_time}}</td>
                                     <td>
                                         <route-link :to="{name:'poll.edit', params:{id: poll.id}}" class="btn btn-sm btn-outline-info">Edit</route-link>
                                         <button class="btn btn-sm btn-outline-danger" @click.prevent="destroy(transaction.id, index)">Delete</button>
@@ -39,8 +43,28 @@
 
 
 <script>
-    export default {
+    import axios from 'axios'
+    import { onMounted, ref } from "vue";
 
+    export default {
+        setup() {
+            let poll = ref([]);
+
+            onMounted(() => {
+                axios.get('http://127.0.0.1:8000/api/poll')
+
+                .then((result) => (
+                    poll.value = result.data
+                )).catch((err) => {
+                    console.log(err.response)
+                });
+            });
+
+
+            return {
+                poll
+            }
+        }
     }
 
 </script>
